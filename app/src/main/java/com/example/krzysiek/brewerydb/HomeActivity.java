877fc8fd3ad9ef48868341eb78f86373e723fb1c
Krzysztof Stępnikowski <skrzychu7@gmail.com>
2016-01-28 23:51:07 +0100

    Poprawki

--- app/src/main/java/com/example/krzysiek/brewerydb/HomeActivity.java
+++ app/src/main/java/com/example/krzysiek/brewerydb/HomeActivity.java
@@ -48,14 +48,7 @@ public class HomeActivity extends AppCompatActivity     {
 
         fetchData();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     public void fetchData(){
