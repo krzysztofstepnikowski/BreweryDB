3cc23a241bdd89f5f41560f4abdfebca0d43e40c
Krzysztof Stępnikowski <skrzychu7@gmail.com>
2016-01-30 22:56:00 +0100

    Toggle Button

--- app/src/main/java/com/example/krzysiek/brewerydb/AboutBeerActivity.java
+++ app/src/main/java/com/example/krzysiek/brewerydb/AboutBeerActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.squareup.picasso.Picasso;
 
@@ -19,6 +23,8 @@ public class AboutBeerActivity extends AppCompatActivity {
     private TextView abvBeerTextViewDetails;
     private TextView descriptionBeerTextViewDetails;
     private Button addToFavouriteDetailsButton;
+
+    private ToggleButton mSwitchShowSecure;
 
 
     @Override
@@ -82,6 +88,33 @@ public class AboutBeerActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+
+        mSwitchShowSecure = (ToggleButton) menu.findItem(R.id.favouriteBeersMenuItem).getActionView().findViewById(R.id.switch_show_protected);
+        mSwitchShowSecure.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
